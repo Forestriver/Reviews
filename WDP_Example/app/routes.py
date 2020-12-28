cd8836f909aa320c72cb5c3e884d8236e337ef91
Forestriver <vitaly.romas1@gmail.com>
2020-12-29 01:18:14 +0200

    aiohttp_boilerplate attempt stacktrace

---
+++ WDP_Example/app/routes.py
@@ -0,0 +1,4 @@
+from . import views
+
+def setup_routes(app):
+    app.router.add_route("GET", "/", views.PostListView)
---
+++ WDP_Example/app/schemas.py
@@ -0,0 +1,6 @@
+from marshmallow import Schema, fields
+
+class PostSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    content = fields.Str()
