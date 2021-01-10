8d934110dafaed7fcedfce7704fa0096dfa6a64a
Forestriver <vitaly.romas1@gmail.com>
2021-01-11 00:56:28 +0200

    get by id update

--- review_app/app/routes.py
+++ review_app/app/routes.py
@@ -2,3 +2,4 @@ from . import views
 
 def setup_routes(app):
     app.router.add_route("GET", "/", views.PostListView)
+    app.router.add_route("GET", "/review_id", views.ListById)
--- review_app/app/views.py
+++ review_app/app/views.py
@@ -1,18 +1,23 @@
 from aiohttp_boilerplate.views.create import CreateView
 from aiohttp_boilerplate.views.list import ListView
-from aiohttp_boilerplate.views.list import RetrieveView
+from aiohttp_boilerplate.views.retrieve import RetrieveView
 
 from app import models
 from app import schemas
 
-# Endpont to retrieve data by id
 
-class PostListView(RetrieveView):
+# Endpoint to access the whole list of reviews.
+class PostListView(ListView):
     def get_model(self):
         return models.Post
 
     def get_schema(self):
         return schemas.PostSchema
 
-    def review(self):
-        result = self.get_model.get_by_id(id=2)
+# Endpont to retrieve data by id
+class ListById(RetrieveView):
+    def get_model(self):
+        return models.Post
+
+    async def before_get(self):
+        self.where ='id=2'
